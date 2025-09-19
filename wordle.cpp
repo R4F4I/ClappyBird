ed2139213a4ca06c95e070f0789c488044fde282
R4F4I <94185789+R4F4I@users.noreply.github.com>
2025-09-19 12:48:02 +0500

    completed wordle

--- wordle.cpp
+++ wordle.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <stdlib.h>
 #include <list>
 
 # define COLOR_RESET        "\33[0m"            // ANSI code for COLOR_RESET output
@@ -41,8 +42,22 @@ list <string> listOfWords= {
 "Zebra"
 };
 
+void cursor_up(int X){
+    printf("\033[%dA",X); // Move up X lines;
+}
+
+void cursor_left(int X){
+    printf("\033[%dD",X); // Move left X lines;
+}
+
 // for orange hints
 bool charInString(string src, char target){
+
+    for (char& c: src)
+    {
+        c=toupper(c);
+    }
+
     for (char c: src)
     {
         if (c==target) return true;
@@ -51,8 +66,23 @@ bool charInString(string src, char target){
 }
 
 bool validWord(string src, list<string> list){
+    
     for(const auto& element : list){
-        if (element == src)
+        string newElement = element;
+        for (char& c: src)
+        {
+            c= toupper(c);
+        }
+        
+        for (char& c: newElement)
+        {
+            c= toupper(c);
+        }
+        
+
+
+        // cout<< newElement <<" " << src << endl;
+        if (newElement == src)
         {
             return true;
         }
@@ -60,17 +90,21 @@ bool validWord(string src, list<string> list){
     return false;
 }
 
+bool matchPosition(char c, char d){
+ return c==d;
+}
+
 int main(){
 
     // choose a random word from list
-    string word = "hello";
-    string guessedWords[5] = {"abcde","abcde","","abcde","edcba"};
+    string secretWord = "apple";
+    string guessedWords[5] = {"     ","     ","     ","     ","     "};
     string enteredWord;
 
 
     //////////////////COMPLETED//////////////////
     // convert root word to capital
-    for (char& c: word)
+    for (char& c: secretWord)
     {
         c = toupper(c);
     }
@@ -92,21 +126,34 @@ int main(){
     
 
     //////////////////:: MAIN LOOP :://////////////////
+    system("cls");
 
     // for loop to 5 / for 5 chances in guessing
     for (size_t i = 0; i < 5; i++) // 5 chances
     {
+        cursor_up(8);
+        cursor_left(11);
         
         // print the entered words
         for (size_t j = 0; j < 5; j++) // prints each guess word
         {
+            int k = 0; // ued to comapare the 'word' and entered word char side by side
             for (char& c : guessedWords[j])
             {   
-                if (charInString(enteredWord, c))
+
+                c = toupper(c);
+
+
+                if (matchPosition(c,secretWord[k++]))
+                {
+                    cout<< GREEN<< c << COLOR_RESET<< " ";
+                }                
+                else if (charInString(secretWord, c))
                 {
                     
-                    cout<< YELLOW<< c << " " << COLOR_RESET;
-                }else
+                    cout<< YELLOW<< c << COLOR_RESET<< " ";
+                }
+                else
                 {
                     cout<< c << " ";
                 }
@@ -118,9 +165,22 @@ int main(){
         cout<<endl<<endl;
 
         // prompt
-        cout << ">";
+        cout << "\r\033[2K>>>>>>";
         cin>>enteredWord;
+
+        for (char& c: enteredWord)
+        {
+            c=toupper(c);
+        }
         
+        
+        if (enteredWord == secretWord)
+        {
+            cout<<"CONGRATS!\n";
+            break;
+        }
+        
+
         // check if entered word is valid
         if (validWord(enteredWord, listOfWords))
         {
@@ -129,6 +189,51 @@ int main(){
         {
             cout << "Not a word"<<endl;
         }
+        
+        
+    }
+
+    
+    cursor_up(9);
+    cursor_left(6);
+
+    // print the entered words
+    for (size_t j = 0; j < 5; j++) // prints each guess word
+    {
+        int k = 0; // ued to comapare the 'word' and entered word char side by side
+        for (char& c : guessedWords[j])
+        {   
+
+            c = toupper(c);
+
+
+            if (matchPosition(c,secretWord[k++]))
+            {
+                cout<< GREEN<< c << COLOR_RESET<< " ";
+            }                
+            else if (charInString(secretWord, c))
+            {
+                
+                cout<< YELLOW<< c << COLOR_RESET<< " ";
+            }
+            else
+            {
+                cout<< c << " ";
+            }
+            
+            
+        }
+        cout<<endl;
+    }
+    
+
+    cout<<CYAN<< "---------"<<endl;
+    
+    for (char& c : secretWord)
+    {   
+        c = toupper(c);
+        cout<< CYAN<< c << COLOR_RESET<< " ";
+        
         
         
     }
